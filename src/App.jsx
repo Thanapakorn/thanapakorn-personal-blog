d44aa07d43819ef3ab8418c35d1bef059b58f185
thanapakorn <thanapakorn001@gmail.com>
2024-10-16 15:44:33 +0700

    setup projuct

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <section>
-      <h1>My First React App on Vite</h1>
+      <h1 className="text-red-500">My First React App on Vite</h1>
     </section>
   );
 }
